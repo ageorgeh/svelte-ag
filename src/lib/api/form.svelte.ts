bc9c97fd65ac6e56391ebe0d1667607997ca89e9
Alexander Hornung <aghornung@gmail.com>
2025-06-02 19:17:56 +1000

    Started on form

---
+++ src/lib/api/form.svelte.ts
@@ -0,0 +1,42 @@
+import { superForm, defaults, setError } from 'sveltekit-superforms';
+import { valibot } from 'sveltekit-superforms/adapters';
+import type { ApiRequestFunction, HTTPMethod, ApiSchema, ApiEndpoints, ErrorBody } from 'ts-ag';
+import type * as v from 'valibot';
+
+export type ApiRequestForm<API extends ApiEndpoints> = <
+  Path extends API['path'],
+  Method extends Extract<API, { path: Path }>['method']
+>(
+  path: Path,
+  method: Method
+) => ReturnType<typeof superForm>;
+
+export function createFormFunction<API extends ApiEndpoints, Path extends API['path']>(
+  schemas: Record<API['path'], Record<HTTPMethod, v.GenericSchema>>,
+  request: ApiRequestFunction<API>
+) {
+  return (path: Path, method: Extract<API, { path: Path }>['method']) => {
+    const schema = schemas[path]?.[method];
+    if (schema === undefined) throw new Error('Invalid schema for form');
+
+    // if (typeof schema === 'function') {
+    //   schema = schema();
+    // }
+
+    return superForm(defaults(valibot(schema)), {
+      SPA: true,
+      resetForm: false,
+      validators: valibot(schema),
+      async onUpdate({ form }) {
+        if (!form.valid) return;
+        const res = await request(path, method, form.data);
+
+        if (res.ok === false) {
+          const body = await res.json();
+
+          setError(form, body.field.name, body.field.value, { status: res.status });
+        }
+      }
+    });
+  };
+}
