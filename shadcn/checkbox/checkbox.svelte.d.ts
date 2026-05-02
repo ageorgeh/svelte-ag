8e0e353cf69b185aa45aef4dd9855eef5737b615
Alexander Hornung <aghornung@gmail.com>
2026-05-02 13:28:42 +1000

    fix: improve full-field component

---
+++ shadcn/checkbox/checkbox.svelte.d.ts
@@ -0,0 +1,5 @@
+import { Checkbox as CheckboxPrimitive } from "bits-ui";
+declare const Checkbox: import("svelte").Component<Omit<Omit<CheckboxPrimitive.RootProps, "child">, "children">, {}, "checked" | "ref" | "indeterminate">;
+type Checkbox = ReturnType<typeof Checkbox>;
+export default Checkbox;
+//# sourceMappingURL=checkbox.svelte.d.ts.map
--- src/lib/components/form/form-field-full.svelte
+++ src/lib/components/form/form-field-full.svelte
@@ -1,18 +1,65 @@
+<!--
+@component FormFieldFull
+Renders all elements of a form field. If a simple Input element is all you don't 
+need to provide a child
+
+```svelte
+<ContactField
+    class="z-5 w-full self-start"
+    {form}
+    name="fields.name"
+    inputProps={{ type: 'text', placeholder: 'Enter your name' }}
+    label="Name"
+/>
+```
+
+For custom input elements like TextArea you will need to pass a child. Everything
+else is the same just bind the value prop as so:
+
+```svelte
+<ContactField
+    class="z-5 w-full self-start"
+    {form}
+    name="fields.name"
+    inputProps={{ type: 'text', placeholder: 'Enter your name' }}
+    label="Name"
+>
+    {#snippet child({ props, value })}
+      <Textarea {...props} bind:value={value.get, value.set} class="z-5 w-full" />
+    {/snippet}
+</ContactField>
+
+```
+
+-->
+
 <script module lang="ts">
   export type FormFullFieldProps<T extends Record<string, unknown>, U extends FormPath<T>> = FormFieldProps<T, U> & {
     label: string;
     description?: string;
     inputProps?: HTMLInputAttributes;
+    /**
+     * Pass this snippet to customise the input for the field.
+     * You are responsible for binding to the value eg
+     * <Textarea {...props} bind:value={value.get, value.set} class="z-5 w-full"
+     */
+    child?: Snippet<
+      [
+        Parameters<NonNullable<ComponentProps<typeof FormControl>['children']>>[0] & {
+          value: { get: () => any; set: (v: any) => void };
+        }
+      ]
+    >;
   } & WithoutChildren<WithElementRef<HTMLAttributes<HTMLDivElement>>>;
 </script>
 
 <script lang="ts" generics="T extends Record<string, unknown>, U extends FormPath<T>">
-  import Field, { type FormFieldProps } from './form-field.svelte';
-  import Label from './form-label.svelte';
+  import FormField, { type FormFieldProps } from './form-field.svelte';
+  import FormLabel from './form-label.svelte';
   import Description from './form-description.svelte';
   import FieldErrors from './form-field-errors.svelte';
   import { Input } from '$shadcn/input/index.js';
-  import * as Form from 'formsnap';
+  import { Control as FormControl } from 'formsnap';
   import { type FormPath } from 'sveltekit-superforms';
   import { get, set } from 'radash';
   import { type WithElementRef, type WithoutChildren } from 'svelte-toolbelt';
@@ -20,6 +67,8 @@
   import type { HTMLAttributes, HTMLInputAttributes } from 'svelte/elements';
   import { mergeProps } from 'svelte-toolbelt';
   import { getFormContext } from './form.svelte';
+  import type { ComponentProps, Snippet } from 'svelte';
+  import { Checkbox } from '$shadcn/checkbox/index.js';
 
   let {
     ref = $bindable(null),
@@ -29,23 +78,40 @@
     label,
     description = '',
     inputProps,
+    child,
     ...restProps
   }: FormFullFieldProps<T, U> = $props();
 
   const formData = $derived(form.form);
 </script>
 
-<Field {form} {name} class={cn(className)} {...restProps}>
-  <Form.Control>
+<FormField {form} {name} class={cn(className)} {...restProps}>
+  <FormControl>
     {#snippet children({ props })}
-      {@const inProps = mergeProps({ ...inputProps, ...props }) as typeof props}
-      <Label>{label}</Label>
+      {@const inProps = mergeProps({
+        class: cn(),
+        ...inputProps,
+        ...props
+      }) as typeof props}
+      <FormLabel>{label}</FormLabel>
 
-      <!-- get and set because we need to dynamically index $formData -->
-      <Input {...inProps} bind:value={() => get($formData, name), (v) => ($formData = set($formData, name, v))} />
+      <!-- radash.get and radash.set because we need to dynamically index $formData -->
+      {#if child}
+        {@render child({
+          props: inProps,
+          value: { get: () => get($formData, name), set: (v) => ($formData = set($formData, name, v)) }
+        })}
+      {:else if inputProps?.type === 'checkbox'}
+        <Checkbox
+          {...inProps}
+          bind:checked={() => get($formData, name), (v) => ($formData = set($formData, name, v))}
+        />
+      {:else}
+        <Input {...inProps} bind:value={() => get($formData, name), (v) => ($formData = set($formData, name, v))} />
+      {/if}
     {/snippet}
-  </Form.Control>
+  </FormControl>
 
   <Description>{description}</Description>
   <FieldErrors />
-</Field>
+</FormField>
